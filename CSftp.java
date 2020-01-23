2c770247f9b4efc998cc8f76ad3a00f114b0be37
ayuzou <imaizumi.ayumi@gmail.com>
2020-01-23 09:56:50 -0800

    fix check for commands, handle lower case and upper case

--- CSftp.java
+++ CSftp.java
@@ -19,33 +19,30 @@ public class CSftp {
 
     static final int DEFAULT_PORT = 21;
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         byte cmdString[] = new byte[MAX_LEN];
 
         // Get command line arguments and connected to FTP
         // If the arguments are invalid or there aren't enough of them
-            // then exit.
+        // then exit.
         int portNumber = DEFAULT_PORT;
 
-        if (args.length == 1){
+        if (args.length == 1) {
             portNumber = Integer.parseInt(args[1]);
-        }
-        else if (args.length != ARG_CNT) {
+        } else if (args.length != ARG_CNT) {
             System.out.print("Usage: cmd ServerAddress ServerPort\n");
             return;
         }
-            String hostName = args[0];
+        String hostName = args[0];
 
         try {
-                Socket socket = new Socket(hostName, portNumber);
-                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Socket socket = new Socket(hostName, portNumber);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                String controlresponse = in.readLine();
+            System.out.println("<-- " + in.readLine());
 
-                System.out.println("<-- " + controlresponse);
-
-                for (int len = 1; len > 0;) {
+            for (int len = 1; len > 0; ) {
                 System.out.print("csftp> ");
                 len = System.in.read(cmdString);
                 if (len <= 0)
@@ -58,50 +55,53 @@ public class CSftp {
                 // Commands
 
                 //  user
-                if (inputWords[0].equals("user")) {
+                if (inputWords[0].equals("USER") || inputWords[0].equals("user")) {
                     writer.write(input + "\r\n");
                     writer.flush();
                 }
 
-                // pw
-                else if (inputWords[0].equals("pw")) {
+                // pass
+                else if (inputWords[0].equals("PASS") || inputWords[0].equals("pass")) {
                     writer.write(input + "\r\n");
                     writer.flush();
                 }
 
                 // quit
-                else if (inputWords[0].equals("quit")) {
+                else if (inputWords[0].equals("QUIT") || inputWords[0].equals("quit")) {
                     writer.write(input + "\r\n");
                     writer.flush();
                 }
 
-                // get
-                else if (inputWords[0].equals("get")) {
+                // pasv
+                else if (inputWords[0].equals("PASV") || inputWords[0].equals("pasv")) {
                     writer.write(input + "\r\n");
                     writer.flush();
                 }
 
-                // features
-                else if (inputWords[0].equals("features")) {
+                // retr
+                else if (inputWords[0].equals("RETR") || inputWords[0].equals("retr")) {
                     writer.write(input + "\r\n");
                     writer.flush();
                 }
 
-                // cd
-                else if (inputWords[0].equals("cd")) {
+                //space in the 3rd index has a space ??
+                // feat
+                else if (inputWords[0].equals("FEAT") || inputWords[0].equals("feat")) {
                     writer.write(input + "\r\n");
                     writer.flush();
                 }
 
-                // dir
-                else if (inputWords[0].equals("dir")) {
+                // cwd
+                else if (inputWords[0].equals("CWD") || inputWords[0].equals("cwd")) {
                     writer.write(input + "\r\n");
                     writer.flush();
                 }
 
-
-
-                //space in the 3rd index has a space
+                // list
+                else if (inputWords[0].equals("LIST") || inputWords[0].equals("list")) {
+                    writer.write(input + "\r\n");
+                    writer.flush();
+                }
 
                 else {
                     System.out.println("0x001 Invalid command.");
@@ -114,9 +114,9 @@ public class CSftp {
                     System.out.println("<-- " + fromServer);
                     break;
                 }
-	    }
-	} catch (IOException exception) {
-	    System.err.println("0xFFFE Input error while reading commands, terminating.");
-	}
+            }
+        } catch (IOException exception) {
+            System.err.println("0xFFFE Input error while reading commands, terminating.");
+        }
     }
 }

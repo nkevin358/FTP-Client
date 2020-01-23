5daef5b342063e787f8c5d39d3dc10c93734dd12
ayuzou <imaizumi.ayumi@gmail.com>
2020-01-23 13:30:01 -0800

    remove unneeded if check with args

--- CSftp.java
+++ CSftp.java
@@ -17,23 +17,19 @@ public class CSftp {
     static final int MAX_LEN = 255;
     static final int ARG_CNT = 2;
 
-    static final int DEFAULT_PORT = 21;
-
     public static void main(String[] args) {
         byte cmdString[] = new byte[MAX_LEN];
 
         // Get command line arguments and connected to FTP
         // If the arguments are invalid or there aren't enough of them
         // then exit.
-        int portNumber = DEFAULT_PORT;
-
-        if (args.length == 1) {
-            portNumber = Integer.parseInt(args[1]);
-        } else if (args.length != ARG_CNT) {
+        if (args.length != ARG_CNT) {
             System.out.print("Usage: cmd ServerAddress ServerPort\n");
             return;
         }
+
         String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
 
         try(
             Socket socket = new Socket(hostName, portNumber);
